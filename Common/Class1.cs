dd6445d05358ef498136cf071f90fd64797a4aad
Timofey_V <tim_vi_ov@mail.ru>
2022-12-18 00:31:40 +0300

    add folder 'Common'

---
+++ Common/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace Common;
+public static class Helper
+{
+
+    public static int[,] RandomArrayInt(int[,] arrayInt)
+    {
+
+        Random random = new Random();
+
+        for (var i = 0; i < arrayInt.GetLength(0); i++)
+        {
+            for (var j = 0; j < arrayInt.GetLength(1); j++)
+            {
+                arrayInt[i, j] = random.Next(0, 20); ;
+            }
+        }
+        return arrayInt;
+    }
+}
