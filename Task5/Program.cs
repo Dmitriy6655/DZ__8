d8d0a55ba83f792c9eaa43515f9912d631dcca05
Timofey_Viligdanov <tim_vi_ov@mail.ru>
2022-12-20 15:35:24 +0300

    add Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+using static Common.Helper;
+
+Console.WriteLine("Input size array:");
+
+int m = int.Parse(Console.ReadLine());
+
+int[,] matrixRes = Filling2DArraySpiral(m);
+
+PrintResultArrayInt(matrixRes);
