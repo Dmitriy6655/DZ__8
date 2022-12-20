a65c070642875053bdb7ec8a87930118405345f6
Timofey_Viligdanov <tim_vi_ov@mail.ru>
2022-12-20 15:36:24 +0300

    correct files tsks

--- Common/Class1.cs
+++ Common/Class1.cs
@@ -66,12 +66,117 @@ public static class Helper
         return arrayResult;
     }
 
-    // public static int[] PrintArray(int[] array)
-    // {
-    //     for (int i = 0; i < array.Length; i++)
-    //     {
-    //         Console.WriteLine(array[i]);
-    //     }
-    // }
+
+    public static int[,] RandomArrayIntMatrix(int[,] arrayInt)
+    {
+        Random random = new Random();
+
+        for (var i = 0; i < arrayInt.GetLength(0); i++)
+        {
+            for (var j = 0; j < arrayInt.GetLength(1); j++)
+            {
+                arrayInt[i, j] = random.Next(1, 5); ;
+            }
+        }
+
+        return arrayInt;
+    }
+
+    //печать 3D массива
+    public static void PrintResult3DArrayInt(int[,,] arrayInt)
+    {
+        for (var i = 0; i < arrayInt.GetLength(0); i++)
+        {
+            for (var j = 0; j < arrayInt.GetLength(1); j++)
+            {
+                for (var k = 0; k < arrayInt.GetLength(2); k++)
+                {
+                    Console.Write("\t {0} ({1}, {2}, {3})", String.Join(" ", arrayInt[j, k, i]),j, k, i );
+                    //Console.Write($"{array[i,j]}  ");
+                }
+
+                Console.WriteLine();
+
+            }
+             
+        }
+
+    }
+
+
+//МЕТОД создания массива с неповторяющимися двузначными числами
+public static int[] ArrayMix()
+{
+    int[] arrayLocal = new int[8];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayLocal.Length; i++)
+    {
+        arrayLocal[i] = random.Next(10 + i * 3, 13 + i * 3);
+        arrayLocal[random.Next(0, 8)] = random.Next(50 + (i * 3), 53 + (i * 3));
+
+    }
+    return arrayLocal;
 
 }
+
+
+//МЕТОД заполнения 3D массива неповторяющимися числами из метода ArrayMix()
+public static int[,,] SearchRepetitionsArray(int[] array)
+{
+
+    int[,,] arrayLocal3D = new int[2, 2, 2];
+
+    int count = 0;
+   
+
+    for (int i = 0; i < arrayLocal3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayLocal3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayLocal3D.GetLength(2); k++)
+            {
+
+                arrayLocal3D[i, j, k] = array[count];
+                count++;
+
+            }
+
+
+        }
+
+    }
+
+    return arrayLocal3D;
+}
+
+//создаем МЕТОД заполнения массива по спирали
+public static int[,] Filling2DArraySpiral (int m)
+{
+
+int[,] matrix = new int[m, m];
+
+int f = 1;
+int d = 0;
+int j = 0;
+
+while (f <= m * m)
+{
+    matrix[d, j] = f;
+    if (d <= j + 1 && d + j < m - 1)
+        ++j;
+    else if (d < j && d + j >= m - 1)
+        ++d;
+    else if (d >= j && d + j > m - 1)
+        --j;
+    else
+        --d;
+    ++f;
+}
+return matrix;
+}
+
+
+}
+
+
